fc3041e08a15eebb9b1293e6d4b275049e390d23
Pedro Lopes Ribeiro <96634781+Pedrolrbr@users.noreply.github.com>
2022-02-28 09:27:37 -0300

    Add files via upload

---
+++ question_model.py
@@ -0,0 +1,7 @@
+#1 Create a Question class with __init__ and using two methods: text and answer
+
+class Question:
+
+    def __init__(self, q_text, q_answer):
+        self.text = q_text
+        self.answer = q_answer
